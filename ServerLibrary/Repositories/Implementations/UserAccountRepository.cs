9f3b5158bdc258af9c11202bf0f0377ac5823891
fakeITDevTeam <dev5544455@gmail.com>
2024-01-30 15:15:27 +0700

    create data models, imports package libraries, create db connection

---
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ServerLibrary.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class UserAccountRepository : IUserAccount
+    {
+        public Task<GeneralResponse> CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
